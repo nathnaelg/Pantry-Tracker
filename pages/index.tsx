076bc1710d15fa135218a23437609d67725015d8
nathnaelg <gulelaat88@gmail.com>
2024-08-03 22:58:12 +0300

    Implement conditional rendering for Home page
    - Added `SwipeableEdgeDrawer` component for navigation
    - Displayed `LandingPage` component for non-authenticated users
    - Rendered `PantryManager` component and inventory header for authenticated users
    - Applied styling and layout adjustments for better responsiveness and alignment

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PantryManager from '@/components/PantryManager';
+import SwipeableEdgeDrawer from '@/components/Drawer';
+import LandingPage from '@/components/LandingPage';
+import { useAuth } from '@/context/AuthContext';
+
+const Home = () => {
+    const { user } = useAuth()
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-2  md:p-24 gap-y-7">
+            <SwipeableEdgeDrawer />
+            {!user ? (<>
+                <LandingPage />
+            </>) : (
+                <>
+                    <h1>Inventory Management</h1>
+                    <PantryManager />
+                </>
+            )}
+        </main>
+    );
+};
+
+export default Home;
