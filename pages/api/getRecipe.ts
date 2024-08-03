e328da8f1bbfd37c28f7be3d1b7807cfbb486e8f
nathnaelg <gulelaat88@gmail.com>
2024-08-03 22:56:12 +0300

    Implement API route for generating recipes with OpenRouter
    - Added Next.js API route to handle chat completions requests
    - Configured fetch request to OpenRouter API with authorization and headers
    - Processed request body to include list of ingredients
    - Returned recipe based on ingredients from OpenRouter API response
    - Added error handling for request failures

---
+++ pages/api/getRecipe.ts
@@ -0,0 +1,40 @@
+import { NextApiRequest, NextApiResponse } from "next";
+require("dotenv").config();
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
+	const { items } = req.body;
+
+	try {
+        const response = await fetch(
+					"https://openrouter.ai/api/v1/chat/completions",
+					{
+						method: "POST",
+						headers: {
+							Authorization: `Bearer ${req.headers["authorization"]}`,
+							"HTTP-Referer": `https://pantry-tracker-rho.vercel.app/`,
+							"X-Title": `https://pantry-tracker-rho.vercel.app/`,
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							model: "meta-llama/llama-3.1-8b-instruct:free",
+							messages: [
+								{
+									role: "system",
+									content:
+										"You are a world class chef with the best culinary practices. I will give you the list of all the ingredients and it's quantity I have in my pantry and I want you to only return a delicious recipe made out of these items.",
+								},
+								{ role: "user", content: items },
+							],
+						}),
+					}
+				).then((res) => {
+					return res.json();
+				});
+		return res.status(200).json({ data: response.choices[0].message.content });
+	} catch (error) {
+		throw Error(error as string);
+	}
+}
